6237e93d2850412dd840179d6074e4efdf827b22
gilgil <gilgil@gilgil.net>
2020-10-28 10:31:22 +0900

    First commit from pcap-test.zip

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <pcap.h>
+#include <stdio.h>
+
+void usage() {
+    printf("syntax: pcap-test <interface>\n");
+    printf("sample: pcap-test wlan0\n");
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        usage();
+        return -1;
+    }
+
+    char* dev = argv[1];
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == nullptr) {
+        fprintf(stderr, "pcap_open_live(%s) return nullptr - %s\n", dev, errbuf);
+        return -1;
+    }
+
+    while (true) {
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(handle, &header, &packet);
+        if (res == 0) continue;
+        if (res == -1 || res == -2) {
+            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
+            break;
+        }
+        printf("%u bytes captured\n", header->caplen);
+    }
+
+    pcap_close(handle);
+}
