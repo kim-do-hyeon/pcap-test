84248fb5d6f1d7f2b18d8ebdef803301873ac390
gilgil <gilgil@gilgil.net>
2020-11-30 20:46:13 +0900

    Fix misspelling

--- pcap-test.cpp
+++ pcap-test.cpp
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 struct Param {
-	char *dev{nullptr};
+	char* dev_{nullptr};
 
 	bool parse(int argc, char* argv[]) {
 		if (argc != 2) {
 			usage();
 			return false;
 		}
-		dev = argv[1];
+		dev_ = argv[1];
 		return true;
 	}
 
@@ -25,11 +25,10 @@ int main(int argc, char* argv[]) {
 	if (!param.parse(argc, argv))
 		return -1;
 
-	char* dev = param.dev;
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	pcap_t* pcap = pcap_open_live(param.dev_, BUFSIZ, 1, 1000, errbuf);
 	if (pcap == nullptr) {
-		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", dev, errbuf);
+		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
 		return -1;
 	}
 
