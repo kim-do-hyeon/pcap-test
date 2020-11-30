5b179fc591b584de7eb493360d08d9839c67f735
gilgil <gilgil@gilgil.net>
2020-11-30 20:43:51 +0900

    Keep indent

--- pcap-test.cpp
+++ pcap-test.cpp
@@ -26,24 +26,24 @@ int main(int argc, char* argv[]) {
 		return -1;
 
 	char* dev = param.dev;
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-    if (pcap == nullptr) {
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (pcap == nullptr) {
 		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", dev, errbuf);
-        return -1;
-    }
+		return -1;
+	}
 
-    while (true) {
-        struct pcap_pkthdr* header;
-        const u_char* packet;
-        int res = pcap_next_ex(pcap, &header, &packet);
-        if (res == 0) continue;
-        if (res == -1 || res == -2) {
-            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
-            break;
-        }
-        printf("%u bytes captured\n", header->caplen);
-    }
+	while (true) {
+		struct pcap_pkthdr* header;
+		const u_char* packet;
+		int res = pcap_next_ex(pcap, &header, &packet);
+		if (res == 0) continue;
+		if (res == -1 || res == -2) {
+			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+			break;
+		}
+		printf("%u bytes captured\n", header->caplen);
+	}
 
-    pcap_close(pcap);
+	pcap_close(pcap);
 }
